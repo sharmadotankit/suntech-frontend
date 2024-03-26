f9fba93314394c82aefa0fa80769665bd922c20d
sharmadotankit <sharma.ankit.kamal@gmail.com>
2024-03-26 13:51:44 +0530

    Added env

--- src/routes/CompanyRegister/CompanyRegister.tsx
+++ src/routes/CompanyRegister/CompanyRegister.tsx
@@ -60,7 +60,7 @@ const CompanyRegister = () => {
   // }
 
   
-
+ 
   return (
     <div >
       <h1 className="text-center text-4xl font-bold mb-4" style={{ fontFamily: 'Orbitron, sans-serif' }}>Company Details</h1>
